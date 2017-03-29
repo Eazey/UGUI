dfe750aa54d21dddb5f8801c2b9f0070df208a76
Eazey <eazey@foxmail.com>
2017-03-29 22:09:23 +0800

    Add Demo
    Create demo of Drag & Drop.

---
+++ EventInterface/Script/DragImage.cs
@@ -0,0 +1,61 @@
+﻿/*- - - - - - - - - - - - - - - - - - - - - - - - - -*/
+/*	  Script Editor: Eazey丶亦泽                      
+/*	  Blog   Adress: http://blog.csdn.net/eazey_wj     
+/*	  GitHub Adress: https://github.com/Eazey 		 
+/*- - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+/*	 Either none appetency, or determined to win.    */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Script Overview: 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragImage : MonoBehaviour,
+    IDragHandler,IBeginDragHandler,IEndDragHandler
+    {
+
+    private Image image;
+    private GameObject go;
+
+    void OnEnable()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (image.sprite == null)
+        {
+            Debug.LogError("Current component of 'Image' have none 'Sprite'.");
+            return;
+        }
+
+        go = new GameObject("Draging");
+        go.transform.SetParent(eventData.pointerDrag.transform.parent);
+
+        go.transform.localPosition = Vector3.zero;
+        go.transform.localScale = Vector3.one;
+
+        Image goImg = go.AddComponent<Image>();
+        goImg.sprite = image.sprite;
+        goImg.raycastTarget = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (go == null)
+            return;
+
+        go.transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Destroy(go);
+        go = null;
+    }
+}
---
+++ EventInterface/Script/DropImage.cs
@@ -0,0 +1,51 @@
+﻿/*- - - - - - - - - - - - - - - - - - - - - - - - - -*/
+/*	  Script Editor: Eazey丶亦泽                      
+/*	  Blog   Adress: http://blog.csdn.net/eazey_wj     
+/*	  GitHub Adress: https://github.com/Eazey 		 
+/*- - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+/*	 Either none appetency, or determined to win.    */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* Script Overview: 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropImage : MonoBehaviour,IDropHandler {
+
+    private Image image;
+
+    void OnEnable()
+    {
+        image = GetComponent<Image>();
+        if (image == null)
+            image = gameObject.AddComponent<Image>();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Sprite s = GetSprite(eventData);
+        if (s != null)
+            image.sprite = s;
+    }
+
+    private Sprite GetSprite(PointerEventData eventData)
+    {
+        GameObject goSource = eventData.pointerDrag;
+        if (goSource == null)
+            return null;
+
+        Image imgSource = eventData.pointerDrag.GetComponent<Image>();
+        if (imgSource == null)
+            return null;
+
+        DragImage DragSource = imgSource.GetComponent<DragImage>();
+        if (DragSource == null)
+            return null;
+
+        return imgSource.sprite;
+    }
+}
